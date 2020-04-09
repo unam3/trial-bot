9e5c54c6f5540c725c1b2cde917de552e63ea21a
Flowers Safety <40meta@gmail.com>
2020-04-09 17:08:55 +0600

    command line arguments validation was implemented

--- app/Main.hs
+++ app/Main.hs
@@ -2,10 +2,20 @@ module Main where
 
 import System.Environment (getArgs)
 
+processArgs :: [String] -> Maybe (String, String, String, Int)
+processArgs [token, helpMsg, repeatMsg, echoRepeatNumber] = let {
+    echoRepeatNumberInt = (read echoRepeatNumber :: Int);
+    isInRange n = n > 1 && n < 6;
+} in if or [null token, null helpMsg, null repeatMsg, not $ isInRange echoRepeatNumberInt]
+    then Nothing
+    else Just (token, helpMsg, repeatMsg, echoRepeatNumberInt)
+processArgs _ = Nothing
+
 main :: IO ()
 main = do
     args <- getArgs
     case args of
-        args@[token, help_msg, repeat_msg, echo_repeat_number] -> putStrLn $ show args
-        _ -> putStrLn "error: exactly four arguments needed: token, help_msg, repeat_msg, echo_repeat_number"
-
+        [_, _, _, _] -> putStrLn $ case processArgs args of
+            Just args' -> show args'
+            Nothing -> "some argument passed from command line is wrong"
+        _ -> putStrLn "error: exactly four arguments needed: token, helpMsg, repeatMsg, echoRepeatNumber"
