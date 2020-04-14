abdedfa6a1954f50ae8a5b9eb5ddd9588ebb8f30
Flowers Safety <40meta@gmail.com>
2020-04-14 19:41:20 +0600

    getting "result" field from getUpdates response

--- src/Bot.hs
+++ src/Bot.hs
@@ -7,14 +7,18 @@ module Bot
     ) where
 
 import Control.Monad.IO.Class
-import Data.Aeson (FromJSON, ToJSON, Value)
+import Data.Aeson (FromJSON, ToJSON, Value, Object, Array, (.:))
+import qualified Data.HashMap.Strict as HM
 import Data.Text (Text, append, pack)
 import GHC.Generics (Generic)
 import Prelude hiding (id)
 import Network.HTTP.Req
 
 
-newtype RequestJSON = WithOffset Int deriving (Show, Generic)
+newtype RequestJSON = WithOffset {
+    -- offset :: Double,
+    tiemout :: Int
+} deriving (Show, Generic)
 
 instance ToJSON RequestJSON
 instance FromJSON RequestJSON
@@ -25,10 +29,12 @@ printUpdates (token, helpMsg, repeatMsg, echoRepeatNumber) = let {
     -- https://core.telegram.org/bots/api#available-methods
     apiMethod = "getUpdates";
     tokenSection = append ("bot" :: Text) $ pack token;
+    --getResult :: Object -> Array;
+    getResult = (HM.! "result");
 -- examples section of http://hackage.haskell.org/package/req-3.1.0/docs/Network-HTTP-Req.html#v:req
 } in runReq defaultHttpConfig $ do
-    v <- req POST (https "api.telegram.org" /: tokenSection /: apiMethod) (ReqBodyJson (WithOffset 20)) jsonResponse mempty
-    liftIO $ print (responseBody v :: Value)
+    response <- req POST (https "api.telegram.org" /: tokenSection /: apiMethod) (ReqBodyJson (WithOffset 200)) jsonResponse mempty
+    liftIO . print $ getResult (responseBody response :: Object)
 
 cycleUpdate :: (String, String, String, Int) -> IO ()
 cycleUpdate args = printUpdates args >> cycleUpdate args
