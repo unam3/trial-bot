3f16b4dfa52f83ecddff5f42a3cd4622b51104c7
Flowers Safety <40meta@gmail.com>
2020-04-16 13:18:32 +0600

    extracting update_id from Update objects

--- src/Bot.hs
+++ src/Bot.hs
@@ -7,7 +7,7 @@ module Bot
     ) where
 
 import Control.Monad.IO.Class
-import Data.Aeson (FromJSON, ToJSON, Value, Object, Array, (.:))
+import Data.Aeson (FromJSON (parseJSON), ToJSON, Value, Object, Array, (.:), withObject)
 import qualified Data.HashMap.Strict as HM
 import Data.Text (Text, append, pack)
 import GHC.Generics (Generic)
@@ -24,17 +24,37 @@ instance ToJSON RequestJSON
 instance FromJSON RequestJSON
 
 
+newtype Update = Update {
+    update_id :: Double
+}  deriving (Show, Generic)
+
+instance ToJSON Update
+instance FromJSON Update where
+    parseJSON = withObject "Update" $ \v -> Update
+        <$> v .: "update_id"
+
+
+newtype ResponseJSON = RJSON {
+    --ok :: Bool,
+    result :: [Update]
+} deriving (Show, Generic)
+
+instance ToJSON ResponseJSON
+instance FromJSON ResponseJSON where
+    parseJSON = withObject "ResponseJSON" $ \v -> RJSON
+        <$> v .: "result"
+
 printUpdates :: (String, String, String, Int) -> IO ()
 printUpdates (token, helpMsg, repeatMsg, echoRepeatNumber) = let {
     -- https://core.telegram.org/bots/api#available-methods
     apiMethod = "getUpdates";
     tokenSection = append ("bot" :: Text) $ pack token;
-    --getResult :: Object -> Array;
-    getResult = (HM.! "result");
+    --getResult = (HM.! "result");
 -- examples section of http://hackage.haskell.org/package/req-3.1.0/docs/Network-HTTP-Req.html#v:req
 } in runReq defaultHttpConfig $ do
     response <- req POST (https "api.telegram.org" /: tokenSection /: apiMethod) (ReqBodyJson (WithOffset 200)) jsonResponse mempty
-    liftIO . print $ getResult (responseBody response :: Object)
+    --liftIO . print $ getResult (responseBody response :: Object)
+    liftIO $ print (responseBody response :: ResponseJSON)
 
 cycleUpdate :: (String, String, String, Int) -> IO ()
 cycleUpdate args = printUpdates args >> cycleUpdate args
