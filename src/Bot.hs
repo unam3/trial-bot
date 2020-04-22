9f4b713468f8d936da3ab396e1b44947da0c33ae
Flowers Safety <40meta@gmail.com>
2020-04-22 17:00:03 +0600

    update_id list prints in a loop

--- src/Bot.hs
+++ src/Bot.hs
@@ -16,9 +16,13 @@ import Prelude hiding (id)
 import Network.HTTP.Req
 
 
-newtype RequestJSON = WithOffset {
-    -- offset :: Int32,
+type Offset = Int32
+
+data RequestJSON = WithoutOffset {
     timeout :: Int
+} | WithOffset {
+    timeout :: Int,
+    offset :: Offset
 } deriving (Show, Generic)
 
 instance ToJSON RequestJSON
@@ -26,7 +30,7 @@ instance FromJSON RequestJSON
 
 
 newtype Update = Update {
-    update_id :: Int32
+    update_id :: Offset
 }  deriving (Show, Generic)
 
 instance ToJSON Update
@@ -35,26 +39,38 @@ instance FromJSON Update where
         <$> v .: "update_id"
 
 
-newtype ResponseJSON = RJSON {
-    --ok :: Bool,
+data ResponseJSON = RJSON {
+    ok :: Bool,
     result :: [Update]
+    --result :: ByteString
 } deriving (Show, Generic)
 
 instance ToJSON ResponseJSON
 instance FromJSON ResponseJSON where
     parseJSON = withObject "ResponseJSON" $ \v -> RJSON
-        <$> v .: "result"
+        <$> v .: "ok"
+        <*> v .: "result"
 
-printUpdates :: (String, String, String, Int) -> IO ()
-printUpdates (token, helpMsg, repeatMsg, echoRepeatNumber) = let {
+
+getUpdates :: (String, String, String, Int) -> Maybe Offset -> IO ResponseJSON
+getUpdates (token, helpMsg, repeatMsg, echoRepeatNumber) maybeOffset = let {
     apiMethod = "getUpdates";
     tokenSection = append ("bot" :: Text) $ pack token;
     urlScheme = https "api.telegram.org" /: tokenSection /: apiMethod;
-    body = (ReqBodyJson (WithOffset 20));
+    body = ReqBodyJson (WithoutOffset 20);
     runReqMonad = req POST urlScheme body jsonResponse mempty >>=
-        (\ response -> liftIO $ print (responseBody response :: ResponseJSON));
+        (\ response -> return (responseBody response :: ResponseJSON));
 } in runReq defaultHttpConfig runReqMonad
-    
 
-cycleUpdate :: (String, String, String, Int) -> IO ()
-cycleUpdate args = printUpdates args >> cycleUpdate args
+getUpdateId :: ResponseJSON -> Maybe Offset
+getUpdateId rjson = let {
+    updates = result rjson;
+} in if null updates then Nothing else Just (update_id $ last updates)
+
+cycleUpdate' :: (String, String, String, Int) -> ResponseJSON -> IO ResponseJSON
+cycleUpdate' args rjson = getUpdates args (getUpdateId rjson) >>=
+    \ ioRJSON -> print ioRJSON
+    >> cycleUpdate' args ioRJSON
+
+cycleUpdate :: (String, String, String, Int) -> IO ResponseJSON
+cycleUpdate args = getUpdates args Nothing >>= cycleUpdate' args
