1f3559229026adc3d29fd508e5609043b9d1c574
Flowers Safety <40meta@gmail.com>
2021-03-25 17:07:39 +0600

    Requests tests was derived

--- test/Tg/TgSpec.hs
+++ test/Tg/TgSpec.hs
@@ -1,27 +1,16 @@
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-module Tg.BotSpec where
+module Tg.TgSpec where
 
 import Data.Either (isRight)
-import qualified Data.Map.Strict as M
 import Data.Text (Text)
 import Prelude hiding (id)
 import Test.Hspec
 
 import Tg (getInt, getLatestSupportedUpdateContent, processArgs)
-import Tg.Types (Config(..))
-import Tg.Requests (commandOrText)
 import Tg.Requests.JSON
 
-testConfig :: Config
-testConfig = Config {
-    tokenSection = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11",
-    helpMessage = "helpMessage",
-    repeatMessage = "repeatMessage",
-    numberOfRepeats = "1",
-    numberOfRepeatsMap = M.empty
-}
 
 spec :: Spec
 spec = do
@@ -50,20 +39,6 @@ spec = do
             shouldBe
                 (processArgs ["123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11", "help msg", "repeat msg", "0"])
                 (Left "Some argument passed from command line is wrong.")
-
-    describe "commandOrText" $ do
-        it "returns help message" $
-            shouldBe
-                (commandOrText testConfig "TESTUSERNAME" 123 "/help")
-                (helpMessage testConfig)
-        it "returns repeat message" $
-            shouldBe
-                (commandOrText testConfig "TESTUSERNAME" 123 "/repeat")
-                "@TESTUSERNAME Current number of repeats is 1. repeatMessage"
-        it "returns any other message as is" $
-            shouldBe
-                (commandOrText testConfig "TESTUSERNAME" 123 "/help as is")
-                "/help as is"
 
     describe "getInt" .
         it "returns Int" $ getInt ("5" :: Text) `shouldBe` (5 :: Int)
